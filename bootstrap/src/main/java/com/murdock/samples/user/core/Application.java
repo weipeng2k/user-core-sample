48ae34777846230586dc14d447e66fe7ef0382db
weipeng2k <weipeng2k@126.com>
2021-06-03 00:00:23 +0800

    feat(user-core): basic module setup, include logfile & Application.

---
+++ bootstrap/src/main/java/com/murdock/samples/user/core/Application.java
@@ -0,0 +1,15 @@
+package com.murdock.samples.user.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author weipeng2k 2021年06月02日 下午20:45:07
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
